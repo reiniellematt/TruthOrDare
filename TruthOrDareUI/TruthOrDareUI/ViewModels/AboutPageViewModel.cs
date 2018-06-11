a8524d5d0edc87916f32829736e202a488f50f49
reiniellematt <erecereinielle722@gmail.com>
2018-06-11 17:01:23 +0800

    AboutPage completed

--- TruthOrDareUI/TruthOrDareUI/ViewModels/AboutPageViewModel.cs
+++ TruthOrDareUI/TruthOrDareUI/ViewModels/AboutPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,21 @@ namespace TruthOrDareUI.ViewModels
 {
     public class AboutPageViewModel : BindableBase
     {
-        public AboutPageViewModel()
-        {
+        private readonly INavigationService _navigationService;
 
+        private DelegateCommand _returnCommand;
+
+        public DelegateCommand ReturnCommand =>
+            _returnCommand ?? (_returnCommand = new DelegateCommand(ExecuteReturnCommand));
+
+        public AboutPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        private async void ExecuteReturnCommand()
+        {
+            await _navigationService.NavigateAsync("MainPage");
         }
     }
 }

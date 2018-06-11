142233e737495860027ed7f774512d780f5c41bd
reiniellematt <erecereinielle722@gmail.com>
2018-06-11 12:27:22 +0800

    Circle button renderer for android added

---
+++ TruthOrDareUI/TruthOrDareUI.Android/FlatButtonRenderer.cs
@@ -0,0 +1,18 @@
+﻿using Android.Content;
+using Android.Runtime;
+using System;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(Button), typeof(TruthOrDareUI.Droid.FlatButtonRenderer))]
+namespace TruthOrDareUI.Droid
+{
+    /// <summary>
+    /// Created for enabling border radius and border color
+    /// </summary>
+    public class FlatButtonRenderer : Xamarin.Forms.Platform.Android.ButtonRenderer
+    {
+        public FlatButtonRenderer(Context context) : base(context)
+        {
+        }
+    }
+}
